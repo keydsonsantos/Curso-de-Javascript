35905d50dc6a54c653621adf15ae78faddf94cdc
KEYDSON MELO DOS SANTOS <keydsonmelo@gmail.com>
2022-08-11 15:43:27 -0300

    atividades

---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'Japão'
+console.log(`Vivendo em ${país}`)
+if(país == 'Brasil') {
+    console.log('Você é brasileiro, Tamo junto!')
+} else {
+    console.log('Você é estrangeiro, Bem-vindo!')
+}
