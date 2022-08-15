3115d23a7924acf8946fd12b11b6ebb58e3f7215
KEYDSON MELO DOS SANTOS <keydsonmelo@gmail.com>
2022-08-15 19:48:35 -0300

    pronto

--- aula12/ex014/script.js
+++ aula12/ex014/script.js
@@ -3,15 +3,21 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    //var hora = 12
-    msg.innerHTML = `Agora são ${hora} horas.`
-    if (hora >= 0 && hora <= 18) {
+    //var hora = 19
+    msg.innerHTML = `Agora são ${hora} horas. </br>`
+    if (hora >= 0 && hora <= 12) {
         img.src = 'img/fotomanhã.png'
+        msg.innerHTML += 'Bom dia!'
+        document.body.style.background = '#e2cd9f'
     }
-    else if (hora <= 12 && hora <= 18) {
+    else if (hora >= 12 && hora <= 18) {
         img.src = 'img/fototarde.png'
+        msg.innerHTML += 'Boa tarde!' 
+        document.body.style.background = '#b9846f'
     } else {
-        img.src 'img/fotonoite.png'
+        img.src = 'img/fotonoite.png'
+        msg.innerHTML += 'Boa noite!'
+        document.body.style.background = '#515154'
     }
 }
 
