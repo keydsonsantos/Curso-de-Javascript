ac87dd09969d5980ad9c7206f97813da1af1515b
KEYDSON MELO DOS SANTOS <keydsonmelo@gmail.com>
2022-08-15 19:23:58 -0300

    up

--- aula12/ex014/script.js
+++ aula12/ex014/script.js
@@ -5,5 +5,13 @@ function carregar() {
     var hora = data.getHours()
     //var hora = 12
     msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora <= 18) {
+        img.src = 'img/fotomanhã.png'
+    }
+    else if (hora <= 12 && hora <= 18) {
+        img.src = 'img/fototarde.png'
+    } else {
+        img.src 'img/fotonoite.png'
+    }
 }
 
