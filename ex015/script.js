cc06effaf586d89a0957817635ca70db9c58287d
KEYDSON MELO DOS SANTOS <keydsonmelo@gmail.com>
2022-08-16 12:35:37 -0300

    ajust

--- ex015/script.js
+++ ex015/script.js
@@ -2,43 +2,47 @@ function verificar() {
     var data = new Date() /*data atual do pc. */
     var ano = data.getFullYear() /*os 4 digitos do ano atual do pc.*/
     var fano = document.getElementById('txtano') /*Vai pegar o ano digitado no formulário. */
-    var res = document.getElementById('res') /*daqui sai o resultado após clicar verificar. */
+    var res = document.getElementById('res') /*daqui sai o resultado após*/
+
     if (fano.value.length == 0 || fano.value > ano) {
-        window.alert ('[ERRO] Verifique os dados e tente novamente.')
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
         if (fsex[0].checked) {
-            gênero = 'Homem'
-            if (idade >=0 && idade < 12) {
+           gênero = 'Homem' 
+
+           if (idade >= 0 && idade < 12) {
+            //criança
+            img.setAttribute('src', 'menino.png')
+           } else if (idade < 21) {
+            //jovem
+            img.setAttribute('src', 'meninoadoles.png')
+           } else if (idade < 50) {
+            //adulto
+            img.setAttribute('src', 'homem.png')
+           } else {
+            //idoso
+            img.setAttribute('src', 'homemvelho.png')
+           }
+
+        }else if (fsex[1].checked) {
+            gênero = 'Feminino'
+
+            if (idade >= 0 && idade < 12) {
                 //criança
-                img.setAttribute('src', 'meninobebe')
-            } else if (idade < 21) {
+               } else if (idade < 21) {
                 //jovem
-                img.setAttribute('src', 'meninoadoles.png')
-            } else if (idade < 50) {
+               } else if (idade < 50) {
                 //adulto
-                img.setAttribute('src', 'homem.png')
-            } else {
+               } else {
                 //idoso
-                img.setAttribute('src', 'homemidoso.png')
-            }
-        } else if (fsex[1].checked) {
-            gênero = 'Mulher'
-            if (idade >=0 && idade < 12) {
-                //criança
-                Img.setAttribute('src', 'meninabebe.png')
-            } else if (idade < 21) {
-                //jovem
-            } else if (idade < 50) {
-                //adulto
-            } else {
-                //idoso
-            }
+               }
         }
         res.style.textAlign = 'center'
-        res.innerHTML = `Detectamos ${gênero} com ${idade} anos. `
-        res.appendChild(img)
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
     }
 }
