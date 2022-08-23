0769a5a6d62dd3876677e6e97201bd8a95911f7b
KEYDSON MELO DOS SANTOS <keydsonmelo@gmail.com>
2022-08-23 16:33:20 -0300

    parimp

---
+++ aula16/funcao01.js
@@ -0,0 +1,13 @@
+function parimp(n) {
+    if (n%2 == 0) {
+        return ('Par!')
+    } else {
+        return ('impar')
+    }
+}
+/*let res = parimp(11)
+console.log(res)*/
+
+/* ou */
+
+console.log(parimp(223))
---
+++ aula16/funcao02.js
@@ -0,0 +1,11 @@
+/*function soma(n1, n2) {
+    return n1 / n2
+}
+
+console.log(soma(6,2)) */
+
+function soma(n1=0, n2=0) {//=0 torna a vaga caso não preechida em 0//
+    return n1 + n2
+}
+
+console.log(soma(6,))
