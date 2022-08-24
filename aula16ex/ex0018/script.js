149a2bf79b72e63ab95296d0f1bcadbe339c8239
KEYDSON MELO DOS SANTOS <keydsonmelo@gmail.com>
2022-08-23 21:18:23 -0300

    arq novs

---
+++ aula16ex/ex0018/script.js
@@ -0,0 +1,28 @@
+let num = document.getElementById('fnum')//digitar o numero
+let lista = document.getElementById('flista')//barra de lista
+let res = document.getElementById('res')//aparecer resposta
+let valores = []
+
+function isNumero(n) {
+    if(Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function inLista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function adiconar() {
+    if(isNumero(num.value) && !inLista(num.value, valores)) {
+        window.alert('Tudo ok')
+    } else {
+        window.alert('Valor inválido ou já encontrado na lista')
+    }
+}
